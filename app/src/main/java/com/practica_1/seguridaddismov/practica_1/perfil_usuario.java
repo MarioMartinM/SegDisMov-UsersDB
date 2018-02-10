5a722f8aee8c174c84e83920fe5879c1e15834ca
Mario Martin <mariomartinmoscoso96@gmail.com>
2018-02-10 12:07:33 +0100

    Commit1-10-02-2018

---
+++ app/src/main/java/com/practica_1/seguridaddismov/practica_1/perfil_usuario.java
@@ -0,0 +1,25 @@
+package com.practica_1.seguridaddismov.practica_1;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class perfil_usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+    }
+
+}
