9a4a3394444f062de7996f332785899fe7a71afb
MarioMartinM <mariomartinmoscoso96@gmail.com>
2018-02-28 10:44:14 +0100

    Commit01-28-02

--- app/src/main/java/com/usersdb/sdm/autenticacion.java
+++ app/src/main/java/com/usersdb/sdm/autenticacion.java
@@ -16,6 +16,10 @@ import android.widget.Toast;
 
 import com.usersdb.sdm.sdm.R;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class autenticacion extends AppCompatActivity {
 
     Context context = this;
@@ -27,6 +31,8 @@ public class autenticacion extends AppCompatActivity {
     public static final String Usuario = "key_user";
     public static final String Password = "key_pass";
     SharedPreferences sprefs;
+
+    String salt = "usersDB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,26 +98,39 @@ public class autenticacion extends AppCompatActivity {
                     }
                     // Si existe el usuario
                     else if (cursor.getCount() == 1) {
-                        // se comprueba si la contraseña de la BBDD coincide con la insertada por el usuario
-                        cursor.moveToFirst();
-                        String passwordDB = cursor.getString(cursor.getColumnIndexOrThrow(UsersDBDatabase.TablaUsuariosSesion.COLUMN_NAME_PASSWORD));
+                        String user_sprefs = sprefs.getString(Usuario, "");
+                        String pass_sprefs = sprefs.getString(Password, "");
 
-                        // Si no coinciden, se le indica al usuario que la contraseña no es correcta
-                        if (!passwordDB.equals(password)){
-                            Toast toast = Toast.makeText(context, "La contraseña introducida no es correcta para el usuario indicado", Toast.LENGTH_SHORT);
-                            toast.show();
-                        }
-                        // Si coinciden, se guardan los datos de usuarios en las preferencias (si no son los mismos) y se inicia sesión (se pasa al menú principal)
-                        else {
-                            SharedPreferences.Editor editor = sprefs.edit();
-                            editor.putString(Usuario, usuario);
-                            editor.putString(Password, password);
-                            editor.apply();
-
+                        if (password.equals(pass_sprefs) && usuario.equals(user_sprefs)){
                             Intent paginaPrincipal = new Intent("android.intent.action.INICIO");
                             startActivity(paginaPrincipal);
                             Toast toast = Toast.makeText(context, "¡Bienvenido/a, "+usuario+"!", Toast.LENGTH_SHORT);
                             toast.show();
+                        }
+                        else {
+                            String password_hashed = getSHA512(password, salt);
+
+                            // se comprueba si la contraseña de la BBDD coincide con la insertada por el usuario
+                            cursor.moveToFirst();
+                            String passwordDB = cursor.getString(cursor.getColumnIndexOrThrow(UsersDBDatabase.TablaUsuariosSesion.COLUMN_NAME_PASSWORD));
+
+                            // Si no coinciden, se le indica al usuario que la contraseña no es correcta
+                            if (!passwordDB.equals(password_hashed)) {
+                                Toast toast = Toast.makeText(context, "La contraseña introducida no es correcta para el usuario indicado", Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
+                            // Si coinciden, se guardan los datos de usuarios en las preferencias (si no son los mismos) y se inicia sesión (se pasa al menú principal)
+                            else {
+                                SharedPreferences.Editor editor = sprefs.edit();
+                                editor.putString(Usuario, usuario);
+                                editor.putString(Password, password_hashed);
+                                editor.apply();
+
+                                Intent paginaPrincipal = new Intent("android.intent.action.INICIO");
+                                startActivity(paginaPrincipal);
+                                Toast toast = Toast.makeText(context, "¡Bienvenido/a, " + usuario + "!", Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
                         }
                     }
                 }
@@ -164,10 +183,12 @@ public class autenticacion extends AppCompatActivity {
                     }
                     // Si no existe el usuario, se añade a la BD
                     else if (cursor.getCount() == 0) {
+                        String password_hashed = getSHA512(password, salt );
+
                         SQLiteDatabase db2 = mDbHelper.getWritableDatabase();
                         ContentValues values = new ContentValues();
                         values.put(UsersDBDatabase.TablaUsuariosSesion.COLUMN_NAME_USERNAME, usuario);
-                        values.put(UsersDBDatabase.TablaUsuariosSesion.COLUMN_NAME_PASSWORD, password);
+                        values.put(UsersDBDatabase.TablaUsuariosSesion.COLUMN_NAME_PASSWORD, password_hashed);
                         db2.insert(UsersDBDatabase.TablaUsuariosSesion.TABLE_NAME, null, values);
 
                         Toast toast = Toast.makeText(context, "Se ha añadido el nuevo usuario a la BBDD", Toast.LENGTH_SHORT);
@@ -177,4 +198,33 @@ public class autenticacion extends AppCompatActivity {
             }
         });
     }
+
+    private String getSHA512(String passwordToHash, String salt){
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt.getBytes(StandardCharsets.UTF_8));
+            byte[] bytes = md.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < bytes.length; i++) {
+                String hex = Integer.toHexString(0xFF & bytes[i]);
+                if (hex.length() == 1) {
+                    sb.append('0');
+                }
+                sb.append(hex);
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+
 }
